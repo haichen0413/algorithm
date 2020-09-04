48b60d5a5687fe1709780f2746de3555fce90875
wangpengfei <2987644042@qq.com>
2020-09-04 09:15:47 +0800

    init

---
+++ 100.相同的树.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  // 先判断边界
+  if (q === null && q === null) {
+    return true;
+  }
+  if (q === null || p === null) {
+    return false;
+  }
+
+  if (p.val !== q.val) {
+    return false;
+  }
+
+  // 递归判断
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
